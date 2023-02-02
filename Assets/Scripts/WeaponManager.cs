224fa3a8364d003fb71076a86787184e5c1ef1f4
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-02-02 18:00:14 -0300

    ADD UMP45 Weapon and WeaponManager Script!

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public GameObject[] weapons;
+    private int index;
+    public float switchDelay = 1f;
+    private bool isSwitching;
+
+
+    void Start()
+    {
+        initializeWeapons();
+    }
+
+    // 
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && !isSwitching)
+        {
+            index++;
+
+            if (index >= weapons.Length)
+            {
+                index = 0;
+            }
+
+            StartCoroutine(switchWeaponDelay(index));
+
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && !isSwitching)
+        {
+            index--;
+            if (index < 0)
+            {
+                index = weapons.Length - 1;
+            }
+
+            StartCoroutine(switchWeaponDelay(index));
+        }
+    }
+
+    IEnumerator switchWeaponDelay(int newIndex)
+    {
+        isSwitching = true;
+        yield return new WaitForSeconds(switchDelay);
+        isSwitching = false;
+        SwitchWeapons(newIndex);
+    }
+
+    //
+    void initializeWeapons()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].SetActive(false);
+        }
+        weapons[0].SetActive(true);
+    }
+
+    //
+    void SwitchWeapons(int newIndex)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].SetActive(false);
+        }
+        weapons[newIndex].SetActive(true);
+    }
+}
